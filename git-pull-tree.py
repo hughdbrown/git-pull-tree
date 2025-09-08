f14d65132631a9946a77a201572a14bcd65704c5
Hugh Brown <hughdbrown@yahoo.com>
2025-09-08 17:19:11 -0600

    Initial revision

---
+++ git-pull-tree.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+
+skippable = {
+    '.pytest_cache',
+    '__pycache__',
+    'env',
+    'venv',
+    '.cache',
+    'node_modules',
+    '.fingerprint',
+    'target',
+}
+
+def main(root: Path, traverse_len: int) -> None:
+    # print(f"# {'-' * 20} {traverse_len} {str(root)}")
+    paths = [p for p in root.glob('*') if p.is_dir()]
+    if not paths:
+        return
+
+    pathnames = {p.name for p in paths}
+    if ".git" in pathnames:
+         # Current directory is a git repo
+         print(f"# {'-' * 20} {traverse_len} {str(root)}")
+         print(f"(cd '{str(root)}' && git pull && cd -)")
+         return
+
+    traverse_len += 1
+    for p in paths:
+        if p.stem not in skippable:
+            main(p, traverse_len)
+
+
+if __name__ == '__main__':
+    main(Path().cwd(), 0)
